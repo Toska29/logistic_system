7265634577cafb84e3d09429509f05ec001081e3
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2021-11-09 18:23:41 +0100

    exception handling

---
+++ src/main/java/africa/semicolon/logisticSystem/exceptions/InvalidPackageIdException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.logisticSystem.exceptions;
+
+public class InvalidPackageIdException extends LogisticSystemAppException{
+    public InvalidPackageIdException(String message) {
+        super(message);
+    }
+}
